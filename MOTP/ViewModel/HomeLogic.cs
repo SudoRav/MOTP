563d67b742412dfd917e46febb2c1176804887a9
Sudo Raviro <sudo.ravira@gmail.com>
2026-04-03 14:14:07 +0300

    Добавьте файлы проекта.

---
+++ MOTP/ViewModel/HomeLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+
+namespace MOTP.Logic
+{
+    public static class HomeLogic
+    {
+        // === Автокириллизация ===
+        public static string Cyrillify(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+
+            return s
+                .Replace("A", "А").Replace("B", "В")
+                .Replace("C", "С").Replace("E", "Е")
+                .Replace("H", "Н").Replace("K", "К")
+                .Replace("M", "М").Replace("O", "О")
+                .Replace("P", "Р").Replace("T", "Т")
+                .Replace("X", "Х");
+        }
+
+        // === Проверка корректности строки ===
+        public static bool FormStr(string str, int typeIndex, bool isPlomb)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            // Твой реальный алгоритм проверки можно вставить сюда
+            if (str.Length < 3)
+                return false;
+
+            return true;
+        }
+
+        // === Очистка двух полей ===
+        public static void ClearEntry(Action<string> setNacl, Action<string> setPlomb)
+        {
+            setNacl("");
+            setPlomb("");
+        }
+    }
+}
